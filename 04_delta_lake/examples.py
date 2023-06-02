9e0d5300880c8e0e704f5d229895f8446600362a
mconwa02 <mconwa02@dcs.bbk.ac.uk>
2023-06-02 16:07:39 +0100

    adding spark stuff

---
+++ 04_delta_lake/examples.py
@@ -0,0 +1,16 @@
+from delta.tables import *
+from pyspark.sql.functions import *
+
+
+from delta import *
+
+builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+
+deltaTable = DeltaTable.forPath(spark, "/tmp/table")
+
+deltaTable.delete(condition)
